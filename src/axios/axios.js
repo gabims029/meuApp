62c97a9a83254e64bd43d60e786d72437a5b391c
gabims029 <gabimelo541@gmail.com>
2025-02-26 09:12:07 -0300

    Versão atualizada

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL:"http://10.89.240.69:5000/api/v1/",
+    baseURL:"http://10.89.240.72:5000/api/v1/",
     headers:{
         'accept':'application/json'
     }
